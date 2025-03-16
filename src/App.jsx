bb9ed3a6e42b513368f2f72bf8d188cef3d65da0
Amrutha277 <amrutav.ramireddy@gmail.com>
2025-03-16 19:11:30 -0400

    hashrowter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
